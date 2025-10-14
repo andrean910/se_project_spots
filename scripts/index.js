fead4f0cc2d591bbff5f9f5bd8998eb5f30f0a0e
ANDREA NOONAN <ANDREAN910@GMAIL.COM>
2025-10-14 15:53:55 -0400

    Modal Submission

--- scripts/index.js
+++ scripts/index.js
@@ -29,22 +29,42 @@ const initialCards = [
   },
 ];
 
+//Variables for Edit Profile
 const editProfileBtn = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const closeEditBtn = editProfileModal.querySelector(".modal__close-btn");
+const profileNameEl = document.querySelector(".profile__name");
+const profileJobEl = document.querySelector(".profile__job");
+const editProfileForm = editProfileModal.querySelector("#profile-form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileJobInput = editProfileModal.querySelector(
+  "#profile-desc-input"
+);
 
+//Variables for New Post
+const addPostbtn = document.querySelector(".profile__add-button");
+const newPostModal = document.querySelector("#new-post-modal");
+const closePostBtn = newPostModal.querySelector(".modal__close-btn");
+const postLinkEl = document.querySelector(".card__image");
+const postCaptionEl = document.querySelector(".card__title");
+const newPostForm = newPostModal.querySelector("#post-form");
+const newPostLinkInput = newPostModal.querySelector("#new-post-input");
+const newPostCaptionInput = newPostModal.querySelector("#caption-input");
+
+//Open and Close Edit Profile Modal
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileJobInput.value = profileJobEl.textContent;
 });
 
 closeEditBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
-const addPostbtn = document.querySelector(".profile__add-button");
-const newPostModal = document.querySelector("#new-post-modal");
-const closePostBtn = newPostModal.querySelector(".modal__close-btn");
-
+//Open and Close New Post Modal
 addPostbtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
 });
@@ -56,3 +76,25 @@ closePostBtn.addEventListener("click", function () {
 initialCards.forEach(function (item) {
   console.log(item.name);
 });
+
+//Edit Profile Submit
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileJobEl.textContent = editProfileJobInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
+
+//New Post Submit
+function handlePostFormSubmit(evt) {
+  evt.preventDefault();
+
+  postLinkEl.textContent = newPostLinkInput.value;
+  postCaptionEl.textContent = newPostCaptionInput.value;
+  console.log(postLinkEl);
+  console.log(postCaptionEl);
+  newPostModal.classList.remove("modal_is-opened");
+}
+newPostForm.addEventListener("submit", handlePostFormSubmit);
